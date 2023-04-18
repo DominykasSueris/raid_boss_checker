d8db95b4f7c2c6a0845f8e43be2e79d3e82f484f
Dominykas Sueris <dominykassueris@gmail.com>
2023-04-18 20:40:00 +0100

    SortByStatus function added

--- src/App.tsx
+++ src/App.tsx
@@ -26,14 +26,21 @@ const App = () => {
         return response.json();
       })
       .then((data) => {
-        setRaids(data);
-        console.log(data);
+        setRaids(sortByStatus(data));
       });
   };
 
   useEffect(() => {
     fetchUserData();
   }, []);
+
+  const sortByStatus = (raids: RaidsData[]) => {
+    return raids.sort((a, b) => {
+      return a.date < b.date
+        ? -1
+        : 1 || parseInt(a.status) - parseInt(b.status);
+    });
+  };
 
   const filterByStatus = (raids: RaidsData[]) => {
     setRaids(raids.filter((raid) => raid.status === "1"));
