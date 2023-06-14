da06ebbbd9abac620e51391964147da35b2bcd78
Dominykas Sueris <dominykassueris@gmail.com>
2023-06-14 22:35:38 +0100

    RaidTableHeadCell component created

---
+++ src/components/RaidTableHeaderCell.tsx
@@ -0,0 +1,38 @@
+import { Column } from "../utils/spec";
+import { Sorting } from "./RaidTable";
+
+interface RaidTableHeader {
+  column: Column;
+  sorting: Sorting;
+  sortTable: (sorting: Sorting) => void;
+}
+
+const RaidTableHeaderCell = ({
+  column,
+  sorting,
+  sortTable,
+}: RaidTableHeader) => {
+  const isDescSorting =
+    sorting.column === column.key && sorting.order === "desc";
+  const isAscSorting = sorting.column === column.key && sorting.order === "asc";
+  const futureSortingOrder = isDescSorting ? "asc" : "desc";
+
+  return (
+    <th
+      key={column.key}
+      onClick={() =>
+        column.sortable &&
+        sortTable({ column: column.key, order: futureSortingOrder })
+      }
+    >
+      {column.label}
+      {!isDescSorting && !isAscSorting && (
+        <i className={column.key === "date" ? "null" : "bi bi-arrow-up"}></i>
+      )}
+      {isDescSorting && <i className={"bi bi-arrow-down"}></i>}
+      {isAscSorting && <i className={"bi bi-arrow-up"}></i>}
+    </th>
+  );
+};
+
+export default RaidTableHeaderCell;
