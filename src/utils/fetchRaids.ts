213847b23ea7365ceadb1ec377204062f81b6757
Dominykas Sueris <dominykassueris@gmail.com>
2023-06-14 22:38:25 +0100

    mapToRaidsInfo function created

--- src/utils/fetchRaids.ts
+++ src/utils/fetchRaids.ts
@@ -1,17 +1,30 @@
 import { useEffect, useState } from "react";
-import { RaidsData } from "./spec";
+import { RaidsData, RaidsInfo } from "./spec";
 import { date } from "./date";
-import { sortByStatus } from "./sortArray";
+import NpcList from "../NpcList";
 
 export const useFetchRaids = () => {
-  const [raids, setRaids] = useState<RaidsData[]>([]);
+  const [raids, setRaids] = useState<RaidsInfo[]>([]);
 
   const fetchUserData = async () => {
     const res = await fetch(
       `https://seasons.l2reborn.org/wp-content/uploads/raids/raids.json?${date}`
     );
-    const data = await res.json();
-    setRaids(sortByStatus(data));
+    const data = (await res.json()) as RaidsData[];
+    const raidsInfo = data.map((r) => mapToRaidsInfo(r));
+    setRaids(raidsInfo);
+  };
+
+  const mapToRaidsInfo = (raid: RaidsData): RaidsInfo => {
+    const npc = NpcList.getById(raid.id);
+    const isUp = raid.status === "1";
+    return {
+      id: raid.id,
+      status: isUp ? "On" : "Off",
+      name: npc.name,
+      level: npc.level,
+      date: isUp ? "" : raid.date,
+    };
   };
 
   useEffect(() => {
